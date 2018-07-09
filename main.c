37e6ff076ef98889f0e53710bfe09c91337d917f
dondish <odedshapira1@gmail.com>
2018-07-09 14:50:22 +0300

    added main.c /.h and the CMakeLists

---
+++ main.c
@@ -0,0 +1,5 @@
+//
+// Created by dondish on 7/9/18.
+//
+
+#include "main.h"
---
+++ main.h
@@ -0,0 +1,8 @@
+//
+// Created by dondish on 7/9/18.
+//
+
+#ifndef SAFST_MAIN_H
+#define SAFST_MAIN_H
+
+#endif //SAFST_MAIN_H
