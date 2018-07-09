d7d63da5a23c9ae940bac8f5da669f5b79bda627
dondish <odedshapira1@gmail.com>
2018-07-09 14:56:41 +0300

    no need for main.h

--- main.c
+++ main.c
@@ -1,5 +1,3 @@
 //
 // Created by dondish on 7/9/18.
 //
-
-#include "main.h"
--- main.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by dondish on 7/9/18.
-//
-
-#ifndef SAFST_MAIN_H
-#define SAFST_MAIN_H
-
-#endif //SAFST_MAIN_H
